3f3bf7d365a27a3c9c6c4c2355ff5e7141534c7b
Ramesh <urameshch@gmail.com>
2024-07-14 07:12:03 +0530

    git-steps

--- backend.sh
+++ backend.sh
@@ -43,7 +43,7 @@ else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
-mkdir /app &>>$LOGFILE
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "Creating App Directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
