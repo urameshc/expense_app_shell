2b5a0f98eab87c256a35a3b7f6780a49742e2f5e
Ramesh <urameshch@gmail.com>
2024-07-25 12:08:16 +0530

    git-steps

--- backend.sh
+++ backend.sh
@@ -1,47 +1,6 @@
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+#!/bin/bash
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
-
-dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling NodeJs"
-
-dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enalbing NodeJs 20 Version"
-
-dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Install NodeJs"
-
-id expense &>>$LOGFILE
-if [ $? -ne 0 ]
-then
-    useradd expense &>>$LOGFILE
-    VALIDATE $? "Creating expense user"
-else
-    echo -e "Expense user already created...$Y SKIPPING $N"
-fi
+source ./common.sh
 
 mkdir -p /app &>>$LOGFILE
 VALIDATE $? "Creating App Directory"
--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
+source ./common.sh
 
 VALIDATE(){
    if [ $1 -ne 0 ]
